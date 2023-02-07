b898364a11d292ad697e800c6ade763f53db3f9a
Bhanuprakashvasa <115440020+Bhanuprakashvasa@users.noreply.github.com>
2023-02-07 10:31:18 +0530

    Add files via upload

---
+++ src/components/Welcome.js
@@ -0,0 +1,10 @@
+export function Welcome() {
+    return (
+        <div className='Home'>
+            <h1 className="p-2">Welcome</h1>
+            <div className="d-flex p-3 flex-wrap text-align: start">  
+            "Welcome to the world of CRUD operations! With this tool, you have the ability to easily create, read, update, and delete data with ease. Start managing your information in a simple and intuitive way today!"
+            </div>
+        </div>
+    );
+}
